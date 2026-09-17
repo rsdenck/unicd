71d9f33444a08fa1414aa856651bf693ebd9bd8e
rsdenck <piptr@protonmail.com>
2026-09-17 14:05:07 -0300

    feat(banner): nova identidade visual ASCII art
    - moldura em box-drawing (verde) com arte 'UNIFIQUE CLOUD' (figlet big, azul)
    - subtitulo UNIFIQUE CLOUD DIRECTOR · vCD · OBJECT STORAGE
    - cores mantenidas (verde 191 + azul 39, mais legivel)
    - versao 1.4.2

--- cmd/version.go
+++ cmd/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	Version   = "1.4.1"
+	Version   = "1.4.2"
 	BuildDate = "unknown"
 	Commit    = "unknown"
 )
--- main.go
+++ main.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"strings"
 
 	"github.com/denck/unicd/cmd"
 )
 
 const (
-	ColorAzul  = "\033[38;5;18m"
+	ColorAzul  = "\033[38;5;39m"
 	ColorVerde = "\033[38;5;191m"
 	ColorReset = "\033[0m"
 )
+
+const banner = "\u00a7G\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n" +
+	"\u00a7G\u2551\u00a7R                                                                                               \u00a7G\u2551\n" +
+	"\u00a7G\u2551   \u00a7A _    _ _   _ _____ ______ _____ ____  _    _ ______    _____ _      ____  _    _ _____  \u00a7G   \u2551\n" +
+	"\u00a7G\u2551   \u00a7A| |  | | \\ | |_   _|  ____|_   _/ __ \\| |  | |  ____|  / ____| |    / __ \\| |  | |  __ \\ \u00a7G   \u2551\n" +
+	"\u00a7G\u2551   \u00a7A| |  | |  \\| | | | | |__    | || |  | | |  | | |__    | |    | |   | |  | | |  | | |  | |\u00a7G   \u2551\n" +
+	"\u00a7G\u2551   \u00a7A| |  | | . ` | | | |  __|   | || |  | | |  | |  __|   | |    | |   | |  | | |  | | |  | |\u00a7G   \u2551\n" +
+	"\u00a7G\u2551   \u00a7A| |__| | |\\  |_| |_| |     _| || |__| | |__| | |____  | |____| |___| |__| | |__| | |__| |\u00a7G   \u2551\n" +
+	"\u00a7G\u2551   \u00a7A \\____/|_| \\_|_____|_|    |_____\\___\\_\\\\____/|______|  \\_____|______\\____/ \\____/|_____/ \u00a7G   \u2551\n" +
+	"\u00a7G\u2551\u00a7R                                                                                               \u00a7G\u2551\n" +
+	"\u00a7G\u2551\u00a7R                       UNIFIQUE CLOUD DIRECTOR  \u00b7  vCD  \u00b7  OBJECT STORAGE                      \u00a7G\u2551\n" +
+	"\u00a7G\u255a\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255d"
 
 func initTerminal() {
 	if runtime.GOOS == "windows" {
@@ -22,16 +35,8 @@ func initTerminal() {
 
 func printBanner() {
 	initTerminal()
-	fmt.Println(ColorVerde + "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" + ColorReset)
-	fmt.Println(ColorVerde + "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" + ColorReset)
-	fmt.Println(ColorVerde + ":: " + ColorAzul + "_   _       _  __ _                     ____ _                 _" + ColorVerde + " ::")
-	fmt.Println(ColorVerde + "::" + ColorAzul + "| | | |_ __ (_)/ _(_) __ _ _   _  ___   / ___| | ___  _   _  __| |" + ColorVerde + "::")
-	fmt.Println(ColorVerde + "::" + ColorAzul + "| | | | '_ \\| | |_| |/ _` | | | |/ _ \\ | |   | |/ _ \\| | | |/ _` |" + ColorVerde + "::")
-	fmt.Println(ColorVerde + "::" + ColorAzul + "| |_| | | | | |  _| | (_| | |_| |  __/ | |___| | (_) | |_| | (_| |" + ColorVerde + "::")
-	fmt.Println(ColorVerde + "::" + ColorAzul + " \\___/|_| |_|_|_| |_|\\__, |\\__,_|\\___|  \\____|_|\\___/ \\__,_|\\__,_|" + ColorVerde + "::")
-	fmt.Println(ColorVerde + ":: " + ColorAzul + "                       |_|                                      " + ColorVerde + " ::")
-	fmt.Println(ColorVerde + "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" + ColorReset)
-	fmt.Println(ColorVerde + "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" + ColorReset)
+	b := strings.NewReplacer("§A", ColorAzul, "§G", ColorVerde, "§R", ColorReset).Replace(banner)
+	fmt.Print(b)
 	fmt.Println(ColorVerde + "\n >> Provider CLI ativo e pronto para execução." + ColorReset)
 	fmt.Println()
 }
